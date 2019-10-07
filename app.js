171a8d778c5872ef3032e3a84726c3951d35b3fd
Charles Glass <charlie-glass@live.com>
2019-10-07 13:38:23 -0700

    tested some JS

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-var parentEl = document.getElementById('parentElement');
-parentEl.textContent = 'attempt to contact html';
+// var parentEl = document.getElementById('parentElement');
+// parentEl.textContent = 'attempt to contact html';
 
-var child = document.createElement('h1');
-child.textContent = 'proof of life';
-parentEl.appendChild(child);
+// var child = document.createElement('h1');
+// child.textContent = 'proof of life';
+// parentEl.appendChild(child);
 
-var iceCreamShop = {
-  shopHours: ['6am', '7am', '8am'],
-  hourTotals: ['12', '12', '14'],
-  locationName: 'Pike Place Market',
+// var iceCreamShop = {
+//   shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '2pm', '4pmm', '5pmm', '6pm', '7pm', '8pm'],
+//   hourTotals: ['12', '12', '14'],
+//   locationName: 'Pike Place Market',
 
-  render: function() {
-    for( var i = 0; i < this.shopHours.length; i++ ) {
-      var childEl = document.createElement('li');
-    //   childEl.textContent = 'Shop Hours: ' + this.shopHours[i];
-      childEl.textContent = `Shop Hours: ${this.shopHours[i]} ~ Total Customers: ${this.hourTotals[i]}`;
-      parentEl.appendChild(childEl);
-    }
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
   }
-};
 
-iceCreamShop.render();
+//   render: function() {
+//     for( var i = 0; i < this.shopHours.length; i++ ) {
+//       var childEl = document.createElement('li');
+//       //   childEl.textContent = 'Shop Hours: ' + this.shopHours[i];
+//       childEl.textContent = `Shop Hours: ${this.shopHours[i]} ~ Total Customers: ${this.hourTotals[i]}`;
+//       parentEl.appendChild(childEl);
+//     }
+//   }
+// };
+
+// iceCreamShop.render();
 
